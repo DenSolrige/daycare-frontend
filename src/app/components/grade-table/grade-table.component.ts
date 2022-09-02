b5ab44b25c9c88052b68f3140cbe672f357686b2
Myles0911 <mdwalker1@aggies.ncat.edu>
2022-09-02 12:15:19 -0400

    This commit

--- src/app/components/grade-table/grade-table.component.ts
+++ src/app/components/grade-table/grade-table.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { observable } from 'rxjs';
+import { GradeUtilServiceService } from 'src/app/services/grade-util-service.service';
 
-@Component({
+  @Component({
   selector: 'app-grade-table',
   templateUrl: './grade-table.component.html',
   styleUrls: ['./grade-table.component.css']
 })
 export class GradeTableComponent implements OnInit {
 
-  constructor() { }
+  constructor(private gradeService:GradeUtilServiceService) { }
 
   ngOnInit(): void {
+    (async() => {
+      this.grade = await this.gradeService.createGrade(grade);
+    })
   }
 
 }
